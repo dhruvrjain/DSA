b971330fd49e33c7f4c7f927852501c6b04565cc
Dhruv R Jain <dhruvrjain01@gmail.com>
2023-07-12 23:34:16 +0530

    Day 12

--- Leet_Code/Day_12_Product_Of_Array_Except_Self.cpp
+++ Leet_Code/Day_12_Product_Of_Array_Except_Self.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> a,t;
-        unordered_map<int,int> m;
-
-        int i,prod=1,nzp=1,f,n=nums.size();
-        for(i=0;i<n;i++){
-            m[nums[i]]++;
-        }
+        vector<int> a;
+        int i,n=nums.size();
         int p[n],s[n];
         p[0]=1;
         for(i=1;i<n;i++){
