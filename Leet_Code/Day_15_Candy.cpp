edef48e568d7f76187e3d871bca2e32002014b36
Dhruv R Jain <dhruvrjain01@gmail.com>
2023-07-15 23:05:45 +0530

    Day 15

--- Leet_Code/Day_15_Candy.cpp
+++ Leet_Code/Day_15_Candy.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int i,n=ratings.size(),s=0,ce,f=1,c=0,dir=1;
+        int i,n=ratings.size(),s=0,ce,f=1,c=0;
         int a[n];
         for(i=0;i<n;i++)
         a[i]=1;
-        while(f){
-            f=0;
-            if(dir){
-                for(i=0;i<n;i++)
+        for(i=0;i<n;i++)
+        {
+            if(i>0 && ratings[i-1]<ratings[i])
+            {
+                if(a[i-1]>=a[i])
                 {
-                    if(i>0 && ratings[i-1]<ratings[i])
-                    {
-                        if(a[i-1]>=a[i])
-                        {
-                        a[i]=a[i-1]+1;
-                        f=1;
-                        }
-                    }
-                    if(i<n-1 && ratings[i+1]<ratings[i])
-                    {
-                        if(a[i+1]>=a[i])
-                    { a[i]=a[i+1]+1;
-                        f=1;}
-                    }
+                a[i]=a[i-1]+1;
+                f=1;
                 }
-                dir=0;
             }
-            else{
-                for(i=n-1;i>=0;i--)
+            if(i<n-1 && ratings[i+1]<ratings[i])
+            {
+                if(a[i+1]>=a[i])
+            { a[i]=a[i+1]+1;
+                f=1;}
+            }
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            if(i>0 && ratings[i-1]<ratings[i])
+            {
+                if(a[i-1]>=a[i])
                 {
-                    if(i>0 && ratings[i-1]<ratings[i])
-                    {
-                        if(a[i-1]>=a[i])
-                        {
-                        a[i]=a[i-1]+1;
-                        f=1;
-                        }
-                    }
-                    if(i<n-1 && ratings[i+1]<ratings[i])
-                    {
-                        if(a[i+1]>=a[i])
-                    { a[i]=a[i+1]+1;
-                        f=1;}
-                    }
+                a[i]=a[i-1]+1;
+                f=1;
                 }
-                dir=1;
+            }
+            if(i<n-1 && ratings[i+1]<ratings[i])
+            {
+                if(a[i+1]>=a[i])
+            { a[i]=a[i+1]+1;
+                f=1;}
             }
         }
         for(i=0;i<n;i++)
