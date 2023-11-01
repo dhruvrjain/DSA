38f79cd8fef5f3d164f72b1019f969f12b995e22
Dhruv R Jain <dhruvrjain01@gmail.com>
2023-11-01 20:39:26 +0530

    Day 124

---
+++ Leet_Code/Day_124_Minimum_Path_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int i,j,m=grid.size(),n=grid[0].size();
+        for(i=1;i<n;i++){
+            grid[0][i]+=grid[0][i-1];
+        }
+        for(i=1;i<m;i++){
+            grid[i][0]+=grid[i-1][0];
+        }
+        for(i=1;i<m;i++){
+            for(j=1;j<n;j++){
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
