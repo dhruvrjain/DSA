73ca2d3fca1cd887a78b12fb51c70c31becfb0d8
Dhruv R Jain <dhruvrjain01@gmail.com>
2023-11-16 21:27:21 +0530

    Day 139

---
+++ Leet_Code/Day_139_Find_Unique_Binary_String.cpp
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+    unordered_map<string,bool> m;
+    int n,f=0;
+    string ans;
+    void chk(string s,int l){
+        if(f) return;
+        if(l==n){
+            if(!m[s]){
+                ans=s;
+                f=1;
+            }
+            return;
+        }
+        l++;
+        chk(s+"0",l);
+        chk(s+"1",l);
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        n=nums.size();
+        if(n==1){
+            if(nums[0]=="0") return "1";
+            return "0";
+        }
+        for(string s:nums){
+            m[s]=true;
+        }
+        chk("",0);
+        return ans;
+    }
+};
